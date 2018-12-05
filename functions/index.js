e634949c5759c62b117d5866eb455244207f5d1f
Jupiter191 <liam.h.-@hotmail.com>
2018-12-05 02:04:36 +0000

    Modified index file with custom message

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions');
+const inspect = require('util').inspect;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+// functions.https creates a function that handles HTTP events.
+// The onRequest event handler listens for requests.
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from a Serverless Database!");
+});
