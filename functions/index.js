f6a9c725df31d19934aa3d5b88077e211163bc5d
Jupiter191 <liam.h.-@hotmail.com>
2018-12-05 18:53:26 +0000

    Added cors with WIP addItem endpoint. Added database and admin dependencies

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,24 @@
 const functions = require('firebase-functions');
 const inspect = require('util').inspect;
+const cors = require('cors')({origin: true});
+const admin = require('firebase-admin');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
+
+admin.initializeApp();
+
+const database = admin.database().ref('/items');
+
 // functions.https creates a function that handles HTTP events.
 // The onRequest event handler listens for requests.
-
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from a Serverless Database!");
+});
+
+exports.addItem = functions.https.onRequest((request, response) => {
+    return cors(request, response, () => {
+        
+    });
 });
